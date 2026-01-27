c4aff93692e3d16aff7fd6c0b63d75fb82430ec2
Jazzzny <jazzzny225@gmail.com>
2026-01-27 04:23:58 -0500

    fix backwards conditional

--- js/src/threading/posix/Thread.cpp
+++ js/src/threading/posix/Thread.cpp
@@ -156,7 +156,7 @@ js::ThisThread::SetName(const char* name)
 
   int rv = 0;
 #ifdef XP_DARWIN
-#if !defined(MAC_OS_X_VERSION_10_6) || (MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_6)
+#if defined(MAC_OS_X_VERSION_10_6) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6)
   rv = pthread_setname_np(name);
 #endif
 #elif defined(__DragonFly__) || defined(__FreeBSD__) || defined(__OpenBSD__)
