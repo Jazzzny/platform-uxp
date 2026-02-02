3ccaf5dd08b21d8916159e1191e63b9520b8a97e
Jazzzny <jazzzny225@gmail.com>
2026-02-01 23:04:14 -0500

    Try again, again, again

--- gfx/ycbcr/scale_yuv_argb.cpp
+++ gfx/ycbcr/scale_yuv_argb.cpp
@@ -46,6 +46,24 @@ const YuvConstants* GetYUVConstants(mozilla::YUVColorSpace yuv_color_space,
   }
 }
 
+const YuvConstants* GetYVUConstants(mozilla::YUVColorSpace yuv_color_space,
+                                    mozilla::ColorRange color_range)
+{
+  switch (yuv_color_space) {
+    case mozilla::YUVColorSpace::BT709:
+      return color_range == mozilla::ColorRange::LIMITED
+        ? &libyuv::kYvuH709Constants
+        : &libyuv::kYvuF709Constants;
+
+    default:
+      MOZ_FALLTHROUGH_ASSERT("Unsupported YUVColorSpace");
+    case mozilla::YUVColorSpace::BT601:
+      return color_range == mozilla::ColorRange::LIMITED
+        ? &libyuv::kYvuI601Constants
+        : &libyuv::kYvuJPEGConstants;
+  }
+}
+
 
 // YUV to RGB conversion and scaling functions were implemented by referencing
 // scale_argb.cc
@@ -237,11 +255,21 @@ void YUVBuferIter_Init(YUVBuferIter& iter,
                        mozilla::YUVColorSpace yuv_color_space,
                        mozilla::ColorRange color_range) {
   iter.src_fourcc = src_fourcc;
+#if defined(MOZ_BIG_ENDIAN)
+  const uint8_t* temp_u = iter.src_u;
+  iter.src_u = iter.src_v;
+  iter.src_v = temp_u;
+  int temp_stride = iter.src_stride_u;
+  iter.src_stride_u = iter.src_stride_v;
+  iter.src_stride_v = temp_stride;
+  iter.yuvconstants = GetYVUConstants(yuv_color_space, color_range);
+#else
+  iter.yuvconstants = GetYUVConstants(yuv_color_space, color_range);
+#endif
   iter.y_index = 0;
   iter.src_row_y = iter.src_y;
   iter.src_row_u = iter.src_u;
   iter.src_row_v = iter.src_v;
-  iter.yuvconstants = GetYUVConstants(yuv_color_space, color_range);
 
   if (src_fourcc == FOURCC_I444) {
     YUVBuferIter_InitI444(iter);
